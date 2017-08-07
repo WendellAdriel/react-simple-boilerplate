0bd97f4e8af824250bd285e72da84677d4f99c8b
Wendell Adriel <wendelladriel.ti@gmail.com>
2017-08-07 17:55:12 +0100

    little enhancements

--- src/components/__tests__/Hello.js
+++ src/components/__tests__/Hello.js
@@ -4,8 +4,8 @@ import Hello from 'components/Hello'
 
 describe('Hello (Snapshot)', () => {
   it('Hello renders hello world', () => {
-    const component = renderer.create(<Hello />);
-    const json = component.toJSON();
-    expect(json).toMatchSnapshot();
-  });
-});
+    const component = renderer.create(<Hello />)
+    const json = component.toJSON()
+    expect(json).toMatchSnapshot()
+  })
+})
